c631ed4bb4244ab27a93a556f92b04e7c81b2324
plodeada <plodeada@fit.cvut.cz>
2023-02-24 12:38:28 +0100

    ast start

--- frontend/src/ast.rs
+++ frontend/src/ast.rs
@@ -1,11 +1,48 @@
-pub struct Program {
-
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
 }
 
-pub enum Expr {
+pub enum Val {
+    Integer(i32),
+    Char(char),
+}
 
+pub struct Program {}
+
+pub enum Expr {
+    BinOp(Op, Box<Expr>, Box<Expr>),
+    Value(Val),
+    Call(String, Vec<Expr>),
+    Deref()
 }
 
 pub enum Statement {
+    VarDecl(String),
+    Block(Vec<Statement>),
+    If(Expr, Box<Statement>),
+    IfElse(Expr, Box<Statement>, Box<Statement>),
+}
 
+pub enum PrimType {
+    Int,
+    Char,
+}
+
+pub enum TypeDef {
+    PrimType(PrimType),
+    PointerType(Box<TypeDef>),
+}
+
+pub struct FnDecl {
+    name: String,
+    params: Vec<(String, TypeDef)>,
+    ret_type: TypeDef,
+}
+
+pub struct FnDef {
+    header: FnDecl,
+    body: Statement,
 }
