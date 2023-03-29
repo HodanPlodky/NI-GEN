5ecf28adaf2eb4aaca6b0ec5dcc3309fe9a4366e
HodanPlodky <a.porek@centrum.cz>
2023-03-29 23:37:55 +0200

    IR start

--- middleend/src/lib.rs
+++ middleend/src/lib.rs
@@ -1,3 +1,35 @@
+enum OpCode {
+    Add,
+}
+
+enum RegType {
+    Void,
+    Int,
+}
+
+type Register = usize;
+
+struct BasicBlock {
+    instruction : Vec<Instruction>,
+}
+
+type BBIndex = usize;
+
+enum InstructionType {
+    ImmI(i32),
+    Reg(Register),
+    RegReg(Register, Register),
+    RegRegs(Register, Vec<Register>),
+    Terminator(BBIndex),
+    TerminatorBrach(BBIndex, BBIndex)
+}
+
+pub struct Instruction {
+    reg_type: RegType,
+    name: String,
+    op_code : OpCode,
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
