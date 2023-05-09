de62187809ef5800c925a2b0251ae915a420c549
plodeada <plodeada@fit.cvut.cz>
2023-05-09 21:05:46 +0200

    more backend

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -2,22 +2,66 @@ use middleend::ir::IrProgram;
 
 type Data = Vec<u8>;
 
-type Register = usize;
+type Rd = usize;
+type Imm = i32;
+type Offset = i32;
 
-struct AsmInstruction {
-    Ldi(Register, i32),
-    Addi(Register, Register, i32),
+enum AsmInstruction {
+    Lui(Rd, Imm),
+    Auipc(Rd, Imm),
 
+    Jal(Rd, Offset),
+    Jalr(Rd, Rd, Offset),
+
+    Beq(Rd, Rd, Offset),
+    Bne(Rd, Rd, Offset),
+    Blt(Rd, Rd, Offset),
+    Bge(Rd, Rd, Offset),
+    Bltu(Rd, Rd, Offset),
+    Bgeu(Rd, Rd, Offset),
+
+    Lb(Rd, Rd, Offset),
+    Lh(Rd, Rd, Offset),
+    Lw(Rd, Rd, Offset),
+    Lbu(Rd, Rd, Offset),
+    Lhu(Rd, Rd, Offset),
+
+    Sb(Rd, Rd, Offset),
+    Sh(Rd, Rd, Offset),
+    Sw(Rd, Rd, Offset),
+
+    Addi(Rd, Rd, Imm),
+    Slti(Rd, Rd, Imm),
+    Sltiu(Rd, Rd, Imm),
+    Xori(Rd, Rd, Imm),
+    Ori(Rd, Rd, Imm),
+    Andi(Rd, Rd, Imm),
+    Slli(Rd, Rd, Imm),
+    Srli(Rd, Rd, Imm),
+    Srai(Rd, Rd, Imm),
+
+    Add(Rd, Rd, Rd),
+    Sub(Rd, Rd, Rd),
+    Sll(Rd, Rd, Rd),
+    Srl(Rd, Rd, Rd),
+    Slt(Rd, Rd, Rd),
+    Sltu(Rd, Rd, Rd),
+    Xor(Rd, Rd, Rd),
+    Or(Rd, Rd, Rd),
+    And(Rd, Rd, Rd),
+    Sra(Rd, Rd, Rd),
+
+    
 }
 
 type AsmBasicBlock = Vec<AsmInstruction>;
 
 pub struct AsmProgram {
-    data : Vec<(String, Data)>,
-    text : Vec<AsmBasicBlock>
+    data: Vec<(String, Data)>,
+    text: Vec<AsmBasicBlock>,
 }
 
-pub fn asm_compile(ir_program : IrProgram) -> AsmProgram {
+pub fn asm_compile(ir_program: IrProgram) -> AsmProgram {
     let x = Data::new();
     todo!()
 }
